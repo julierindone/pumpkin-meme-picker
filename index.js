ee30d23c93c9cec082388ef93a993e13b4f5e78b
Julie Rindone <julie.rindone@gmail.com>
2024-05-19 16:30:17 -0500

    Moved loop to renderEmotionsRadios and calls on createEmotionRadio. (My work... might get redone after cont' tutorial)

--- index.js
+++ index.js
@@ -15,32 +15,33 @@ function getEmotionsArray(cats) {
 getEmotionsArray(catsData)
 
 function renderEmotionsRadios(cats) {
-  createEmotionRadio(cats)
-}
-
-function createEmotionRadio(cats) {
-  let emotionRadioDiv = document.getElementById("emotion-radio-div")
-  let radioInput = ""
-  let emotionLabel = ""
   const emotions = getEmotionsArray(cats)
   for (let emotion of emotions) {
     console.log(emotion)
-    // create a div to hold each radio.
-    emotionRadioDiv = document.createElement("div")
-    emotionRadioDiv.className = "radio"
-    // create an emotion input
-    radioInput = document.createElement("input")
-    radioInput.type = "radio"
-    radioInput.id = emotion
-    radioInput.value = emotion
-    radioInput.name = "emotions"
-    // create label - emotionLabel
-    emotionLabel = document.createElement("label")
-    emotionLabel.innerHTML = emotion
-    emotionRadios.appendChild(emotionRadioDiv)
-    emotionRadioDiv.appendChild(radioInput)
-    emotionRadioDiv.appendChild(emotionLabel)
+    createEmotionRadio(cats, emotion)
   }
+}
+function createEmotionRadio(cats,emotion) {
+  let emotionRadioDiv = document.getElementById("emotion-radio-div")
+  let radioInput = ""
+  let emotionLabel = ""
+  // create a div to hold each radio.
+  emotionRadioDiv = document.createElement("div")
+  emotionRadioDiv.className = "radio"
+  // create an emotion input
+  radioInput = document.createElement("input")
+  radioInput.type = "radio"
+  radioInput.id = emotion
+  radioInput.value = emotion
+  radioInput.name = "emotions"
+  // create label - emotionLabel
+  emotionLabel = document.createElement("label")
+  emotionLabel.innerHTML = emotion
+
+  // assemble the pieces:
+  emotionRadios.appendChild(emotionRadioDiv)
+  emotionRadioDiv.appendChild(radioInput)
+  emotionRadioDiv.appendChild(emotionLabel)
 }
 
 renderEmotionsRadios(catsData)
