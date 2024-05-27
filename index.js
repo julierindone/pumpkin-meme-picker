61cef523995bd4ae6d7d219f406514f78868e633
Julie Rindone <julie.rindone@gmail.com>
2024-05-27 10:25:06 -0500

    Removed duplicates from emotion list

--- index.js
+++ index.js
@@ -16,32 +16,39 @@ getEmotionsArray(catsData)
 
 function renderEmotionsRadios(cats) {
   const emotions = getEmotionsArray(cats)
+  let emotionsArray = []
   for (let emotion of emotions) {
-    console.log(emotion)
+    if (!(emotionsArray.includes(emotion))) {
+      emotionsArray.push(emotion)
     createEmotionRadio(cats, emotion)
+    } // no need for else since nothing happens with it!
   }
 }
 function createEmotionRadio(cats,emotion) {
   let emotionRadioDiv = document.getElementById("emotion-radio-div")
   let radioInput = ""
   let emotionLabel = ""
+
   // create a div to hold each radio.
   emotionRadioDiv = document.createElement("div")
   emotionRadioDiv.className = "radio"
+
   // create an emotion input
   radioInput = document.createElement("input")
   radioInput.type = "radio"
   radioInput.id = emotion
   radioInput.value = emotion
   radioInput.name = "emotions"
+
   // create label - emotionLabel
   emotionLabel = document.createElement("label")
+  emotionLabel.htmlFor = emotion
   emotionLabel.innerHTML = emotion
 
   // assemble the pieces:
   emotionRadios.appendChild(emotionRadioDiv)
-  emotionRadioDiv.appendChild(radioInput)
   emotionRadioDiv.appendChild(emotionLabel)
+  emotionRadioDiv.appendChild(radioInput)
 }
 
 renderEmotionsRadios(catsData)
