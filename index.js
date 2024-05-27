1e83bd37903a3fc90d4ab44a3a9907bf99fc8d56
Julie Rindone <julie.rindone@gmail.com>
2024-05-27 11:51:24 -0500

    Switched from using createElements to a block of HTML

--- index.js
+++ index.js
@@ -1,62 +1,43 @@
-import { catsData } from "./data.js"
+import { catsData } from './data.js'
 
-const emotionRadios = document.getElementById("emotion-radios")
+// This is the visible list of moods.
+const emotionRadios = document.getElementById('emotion-radios')
 
 getEmotionsArray(catsData)
 
 renderEmotionsRadios(catsData)
 
-emotionRadios.addEventListener("click", function (e) {
-  // e.target.id(emotion)
-  console.log(`clicked on ${e.target.id}`);
-  
-})
-
+// Gets full list from data.js and filters out dupes
 function getEmotionsArray(cats) {
+
   const emotionsArray = []
   for (let cat of cats) {
     for (let emotion of cat.emotionTags) {
-      emotionsArray.push(emotion)
+      if (!(emotionsArray.includes(emotion))) {
+        emotionsArray.push(emotion)
+      }
     }
   }
+  console.log(`Contents of emotionsArray returned from getEmotionsArray: ${emotionsArray} `);
+
   return emotionsArray
 }
 
+// Creates HTML and prints to screen
 function renderEmotionsRadios(cats) {
+
+  let radioItems = ``
   const emotions = getEmotionsArray(cats)
-  let emotionsArray = []
-  for (let emotion of emotions) {      
-    if (!(emotionsArray.includes(emotion))) {
-      emotionsArray.push(emotion)
-      createEmotionRadio(cats, emotion)
-    } // no need for else since nothing happens with it!
+  for (let emotion of emotions) {
+    radioItems += `
+      <div class="radio">
+        <label for="${emotion}">${emotion}</label>
+        <input 
+        type="radio" 
+        id="${emotion}" 
+        value="${emotion}" 
+        name="emotions">
+      </div>`
   }
+  emotionRadios.innerHTML = radioItems
 }
-
-function createEmotionRadio(cats,emotion) {
-  let emotionRadioDiv = document.getElementById("emotion-radio-div")
-  let radioInput = ""
-  let emotionLabel = ""
-
-  // create a div to hold each radio.
-  emotionRadioDiv = document.createElement("div")
-  emotionRadioDiv.className = "radio"
-
-  // create an emotion input
-  radioInput = document.createElement("input")
-  radioInput.type = "radio"
-  radioInput.id = emotion
-  radioInput.value = emotion
-  radioInput.name = "emotions"
-
-  // create label - emotionLabel
-  emotionLabel = document.createElement("label")
-  emotionLabel.htmlFor = emotion
-  emotionLabel.innerHTML = emotion
-
-  // assemble the pieces:
-  emotionRadios.appendChild(emotionRadioDiv)
-  emotionRadioDiv.appendChild(emotionLabel)
-  emotionRadioDiv.appendChild(radioInput)
-}
-
