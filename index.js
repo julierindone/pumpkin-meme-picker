f67fcfb38d74cd54f6cf147e2f3f4b6f9c00fa4d
Julie Rindone <julie.rindone@gmail.com>
2024-06-08 09:51:21 -0500

    Added event listeners for getImageBtn and  gifsOnlyOption, started setting up getMatchingCatsArray function

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ import { catsData } from './data.js'
 
 // This is the visible list of moods.
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 getEmotionsArray(catsData)
-
 renderEmotionsRadios(catsData)
 
+
+// Notes
 // Listens for whatever radio is chosen. 
 // Using 'change' instead of 'click' bc the id will apply to the whole div, not just the radio.
 // We don't use the () in a named function inside an event listener bc if we did, it would try to invoke it immediately.
+
 emotionRadios.addEventListener('change', highlightCheckedOption)
+getImageBtn.addEventListener('click', getMatchingCatsArray)
+gifsOnlyOption.addEventListener('change', function () {
+  // do I need anything inside this event listener???
+  console.log(`gifsOnlyOption checked? ${gifsOnlyOption.checked}`);
+})
 
 // Gets full list from data.js and filters out dupes
 function getEmotionsArray(cats) {
@@ -23,8 +32,6 @@ function getEmotionsArray(cats) {
       }
     }
   }
-  console.log(`Contents of emotionsArray returned from getEmotionsArray: ${emotionsArray} `);
-
   return emotionsArray
 }
 
@@ -53,3 +60,23 @@ function highlightCheckedOption(e) {
   };
   document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
+
+function getMatchingCatsArray() {
+  const radios = document.getElementsByClassName('highlight')
+  const isGifsOnlySelected = gifsOnlyOption.checked
+
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+    console.log(`selected ${selectedEmotion}`);
+  }
+
+  if (isGifsOnlySelected) {
+    // return array of ONLY gifs from that mood
+    console.log(`Only returning gifs.`)
+  }
+  else {
+    console.log(`Returning gifs and stills.`);
+    // return array of ALL images from that mood
+  }
+}
+
